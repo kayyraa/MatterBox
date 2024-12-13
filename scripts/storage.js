91fc07f243ef46bb8e9da25b22d51938e4fc478a
kayyraa <zindhukayrabasbay@gmail.com>
2024-12-13 20:25:24 +0300

    Cleared all files

--- scripts/storage.js
+++
@@ -1,9 +0,0 @@
-import * as Api from "./api.js";
-
-const Observer = new MutationObserver(() => {
-    console.log(Api.ParticleContainer.children.length);
-});
-
-Observer.observe(Api.ParticleContainer, {
-    childList: true,
-});
